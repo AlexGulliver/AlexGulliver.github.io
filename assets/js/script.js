59ed7f07c718d8dca6833f82f719dc64c88ff8aa
Alex Gulliver <alexgulliver@Alexs-MacBook-Pro.local>
2024-03-11 20:56:07 +0000

    cooked

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,20 @@ document.querySelectorAll(".nav-link").forEach(n => n.
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
     }))
+
+var links = [
+        "bbc.com",
+        "google.com",
+        "youtube.com",
+        "facebook.com"];
+
+generateRandomPlaylist = function () {
+    // get a random number between 0 and the number of links
+    var randIdx = Math.random() * links.length;
+    // round it, so it can be used as array index
+    randIdx = parseInt(randIdx, 10);
+    // construct the link to be opened
+    var link = 'http://' + links[randIdx];
+    // open it in a new window
+    window.open(link);
+};
